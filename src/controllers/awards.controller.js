97c1e68b73f1893e9a64565ffd5fbbaf2e197102
84796578ran <84796578foru.com>
2023-11-17 13:03:22 +0700

    first commit

---
+++ src/controllers/awards.controller.js
@@ -0,0 +1,18 @@
+import { db } from "../database/connect.js";
+import { config as configDotenv } from "dotenv";
+
+
+const award = (req, rep) => {
+   const award = rep.param.award;
+   const query = "";
+
+   db.query(
+    
+   )
+}
+
+app.get('/checkUserLikedBlog', (req,rep) => {
+        res.send("Get liek success")
+})
+
+
